18e5fb9f520052695e60dd51586085897916f71a
STravinJ <stravinpack@yandex.ru>
2022-11-28 00:31:20 +0300

    fix: исправить ошибки pull request

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.booking;
 
-import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingResponseDto;
