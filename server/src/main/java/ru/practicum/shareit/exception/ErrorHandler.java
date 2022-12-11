930b970c228f7395757b0e1f45405061630b355c
STravinJ <stravinpack@yandex.ru>
2022-12-11 20:49:08 +0300

    feat: разработка микро-контроллера

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,12 +1,12 @@
 package ru.practicum.shareit.exception;
 
+import javax.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class ErrorHandler {
